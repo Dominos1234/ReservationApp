e06cc2f198a31aeebe6cb658896897af09a0668a
Dominos <dominikst998@gmail.com>
2020-12-06 14:40:29 +0100

    Solution to network issues

--- Inzynierka/app/src/main/java/com/example/inzynierka/MainActivity.kt
+++ Inzynierka/app/src/main/java/com/example/inzynierka/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.inzynierka
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.StrictMode
 import android.util.Log
 import android.view.Gravity
 import android.view.View
@@ -9,9 +11,8 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.github.kittinunf.fuel.Fuel
-import com.github.kittinunf.fuel.core.extensions.jsonBody
 import com.github.kittinunf.fuel.json.responseJson
-import kotlin.reflect.typeOf
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +21,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (Build.VERSION.SDK_INT > 9) {
+            val policy =
+                StrictMode.ThreadPolicy.Builder().permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+        }
 
     }
 
@@ -33,38 +39,35 @@ class MainActivity : AppCompatActivity() {
         val password = findViewById<EditText>(R.id.password).text.toString()
 
 
-        var toast = Toast.makeText(applicationContext, "$password", Toast.LENGTH_LONG)
-        toast.setGravity(Gravity.TOP, 0, 140)
-        toast.show()
-
 
         val bodyJson = """
           { "login" : "arek",
             "password" : "arek"
           }
         """
-        val requestParams: MutableList<Pair<String, String>> = ArrayList(2)
-        requestParams.add(Pair("login", username))
-        requestParams.add(Pair("password", password))
 
 
-        val (request, response, result) = Fuel.post("https://51.178.82.249:2067/api/user/login")
+        val json = JSONObject()
+        json.put("login", "arek")
+        json.put("password", "arek")
+
+        val (request, response, result) = Fuel.post("http://51.178.82.249:2067/api/user/login")
+            //.header("Content-Type", "application/json")
+            //.header(username,password)
             .body(bodyJson)
             .responseJson()
 
-        var myData = ""
-        result.fold(success = {
-            myData = it.toString()
-        }, failure = {
-            myData = it.errorData.toString()
+        result.fold(success = { json ->
+            Log.d("Success", json.array().toString())
+        }, failure = { error ->
+            Log.e("Failure", error.toString())
         })
 
         val (data, error) = result
 
         val jsonArray = data?.array()
 
-        Log.e("cos", error.toString())
-        toast = Toast.makeText(applicationContext, "Welcome $jsonArray!", Toast.LENGTH_LONG)
+        val toast = Toast.makeText(applicationContext, "Welcome $jsonArray!", Toast.LENGTH_LONG)
         toast.setGravity(Gravity.TOP, 0, 140)
         toast.show()
         showResourcesActivity()
